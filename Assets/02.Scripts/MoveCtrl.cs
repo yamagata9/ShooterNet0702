b7844548820d647bb2caeb656b2b6d3ef6f68291
yamagata9 <akihito1995220@gmail.com>
2015-07-03 12:16:14 +0900

    NetworkManager作成

---
+++ Assets/02.Scripts/MoveCtrl.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveCtrl : MonoBehaviour {
+	//コンポーネントを割り当てる変数を宣言
+	private Transform tr;
+	private CharacterController _controller;
+	
+	//キーボードからの入力値の変数を宣言
+	private float h = 0.0f;
+	private float v = 0.0f;
+	
+	//移動速度と回転速度の変数
+	public float movSpeed = 5.0f;
+	public float rotSpeed = 50.0f;
+	
+	//移動方向のベクトル変数
+	private Vector3 movDir = Vector3.zero;
+	
+	void Start(){
+		//Update関数からアクセスするコンポーネントを変数に代入
+		tr = GetComponent<Transform>();
+		_controller = GetComponent<CharacterController>();	
+	}
+	
+	void Update(){
+		//キーボードからの入力を受け取る
+		h = Input.GetAxis("Horizontal");
+		v = Input.GetAxis("Vertical");
+		
+		//マウスの左右入力値で回転
+		tr.Rotate( Vector3.up * Input.GetAxis("Mouse X") * rotSpeed * Time.deltaTime );
+		
+		//ベクトルの加算演算を利用してからあらかじめ移動方向を計算する
+		movDir = (tr.forward * v) + (tr.right * h);
+		//重力の影響を受けて下に落ちるようにy値を変更
+		movDir.y -= 20f * Time.deltaTime;
+		
+		//プレイヤーを移動させる
+		_controller.Move( movDir * movSpeed * Time.deltaTime );
+	}
+}
---
+++ Assets/02.Scripts/NetWorkMgr.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NetWorkMgr : MonoBehaviour {
+	
+	// 接続IP
+	private const string ip = "127.0.0.1";
+	// 接続ポート
+	private const int port = 30000;
+	// NAT機能の使用の有無
+	private bool _useNat = false;
+	
+	void OnGUI(){
+		// 現在のユーザーのネットワーク接続の有無を判断
+		if( Network.peerType == NetworkPeerType.Disconnected )
+		{
+			//　ゲームサーバー生成ボタン
+			if( GUI.Button ( new Rect( 20, 20, 200, 25 ), "Start Server" ) )
+			{
+				//ゲームサーバー生成 : InitializeServer（接続者数、ボート番号、NATの使用の有無）
+				Network.InitializeServer( 20, port, _useNat );
+			}
+			//
+			if( GUI.Button ( new Rect ( 20, 50, 200, 25 ), "Connect to Server" ) )
+			{
+				Network.Connect(ip, port);
+			}
+		}else{
+			//
+			if( Network.peerType == NetworkPeerType.Server )
+			{
+				GUI.Label( new Rect ( 20, 20, 200, 25 ), "Initialization Server" );
+			}
+			//
+			if( Network.peerType == NetworkPeerType.Client )
+			{
+				GUI.Label( new Rect ( 20, 20, 200, 25 ), "Initialization Server" );
+			}
+		}
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
